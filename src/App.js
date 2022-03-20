26d4e3e78dae1a9e3bbcf3baf2de7b24dcf71933
Eliran Abuhatzira <elirana1989@gmail.com>
2022-03-21 01:27:58 +0200

    Add option to light/dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import * as React from 'react';
 import './App.css';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import Header from './components/Header/Header'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
@@ -10,9 +14,20 @@ import {
 } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
+  
+  
 
+  const theme = createTheme({
+    palette: {
+      mode: 'light'
+    },
+  })
+
+
+  
+  return (
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
     <BrowserRouter>
 
       <Header/>
@@ -24,7 +39,7 @@ function App() {
     </BrowserRouter>
 
 
-    </div>
+    </ThemeProvider>
   );
 }
 
