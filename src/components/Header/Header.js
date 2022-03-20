36ca3da7fd3571150260bbfc8cf0d5e2095f9be7
Eliran Abuhatzira <elirana1989@gmail.com>
2022-03-20 17:10:57 +0200

    Create header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import classes from './Header.module.css';
-import Nav from './Nav/Nav'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import classes from './Header.module.css'
 
 
 
 export default function Header() {
   return (
-    <div className={classes.Header}>
-        <div>Weather</div>
-        <div>
-            <Nav/>
-        </div>
-    </div>
-  )
+      <AppBar className={classes.Header}>
+        <Toolbar>
+          <Typography variant="h5" component="div"  sx={{ flexGrow: 2 }}>
+            Weather
+          </Typography>
+          <Button color="inherit" className={classes.Button+" , "+classes.Active} sx={{ flexGrow:1}}>Home</Button>
+          <Button color="inherit" className={classes.Button} sx={{ flexGrow:1}}>Favorites</Button>
+        </Toolbar>
+      </AppBar>
+  );
 }
