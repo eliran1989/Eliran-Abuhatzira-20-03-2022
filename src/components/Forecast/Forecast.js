6df6a61e610ddcc19f283c9acd093cba57b4e16b
Eliran Abuhatzira <elirana1989@gmail.com>
2022-03-21 01:43:14 +0200

    Init forcast layout

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import classes from './Forecast.module.css'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { Avatar, Paper, Skeleton, Typography } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 export default function Forecast() {
   return (
-    <div>Forecast</div>
+        <Card>
+                <CardContent className={classes.Content}>
+                    <Typography variant="h5" component="div">
+                        Tel-Aviv
+                    </Typography>
+                    <FavoriteBorderOutlinedIcon sx={{ fontSize: 30 , color:"#ff5151" }}/>
+
+                </CardContent>
+                <CardContent className={[classes.Content , classes.Forecast].join(" , ")} >
+                    <Paper/>
+                    <Paper/>
+                    <Paper/>
+                    <Paper/>
+                    <Paper/>
+                </CardContent>
+
+        </Card>
   )
 }
