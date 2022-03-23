9ed827140d4e352e0e3bbe7b9912240c19e52757
Eliran Abuhatzira <elirana1989@gmail.com>
2022-03-23 22:47:42 +0200

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Forecast from '../components/Forecast/Forecast';
 import Container from '@mui/material/Container';
+import SearchInput from '../components/Forecast/SearchInput';
 
 export default function Home() {
   return (
     <Container sx={{marginTop:"10px"}} maxWidth="xl">
+        <SearchInput/>
         <Forecast/>
     </Container>
   )
