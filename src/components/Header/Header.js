b838c8505beef70461a10cf69f10c6777060dcf6
Eliran Abuhatzira <elirana1989@gmail.com>
2022-03-23 22:47:09 +0200

    Handler nav links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import classes from './Header.module.css'
+import { Tab, Tabs } from '@mui/material';
+import { Link, useLocation  } from 'react-router-dom';
 
 
 
 export default function Header() {
+
+  const location = useLocation();
+
+  
+
   return (
-      <AppBar color="default" className={classes.Header} position="sticky">
+      <AppBar color="default" position="sticky">
         <Toolbar>
-          <Typography variant="h6" component="div" className={classes.Title}>
+          <Typography variant="h6" component="div" sx={{flexGrow:1}}>
             Weather
           </Typography>
-          <Button className={classes.Button}>Home</Button>
-          <Button className={classes.Button}>Favorites</Button>
+              <Tabs value={location.pathname}>
+                <Tab label="Home"  value="/" component={Link} to="/"/>
+                <Tab label="Favorites"  value="/favorites" component={Link} to="/favorites" />
+            </Tabs>
         </Toolbar>
       </AppBar>
   );
