3fb2d570b30ba1fd35459f77cbb35778ebc903d9
Eliran Abuhatzira <elirana1989@gmail.com>
2022-03-26 14:14:02 +0300

    Chnage default switch mode button by current theme mode

--- src/components/UI/SideDrawer/SideDrawer.js
+++ src/components/UI/SideDrawer/SideDrawer.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Box, Drawer, FormControlLabel, FormGroup, Switch} from '@mui/material';
 import classes from './SideDrawer.module.css'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { uiActions } from '../../../store/ui-slice';
 
 export default function SideDrawer({status ,toggleDrawer}) {
 
 
-
   const dispatch = useDispatch();
-
+  const themeMode = useSelector((state) =>state.ui.themeMode);
 
   return (
     <Drawer
@@ -19,7 +18,7 @@ export default function SideDrawer({status ,toggleDrawer}) {
     >
     <Box className={classes.Drawer}>
     <FormGroup className={classes.FormGroup}>
-      <FormControlLabel control={<Switch onChange={()=>dispatch(uiActions.toggleThemeMode())}/>} label="Dark mode" />
+      <FormControlLabel control={<Switch {...(themeMode=="dark") ? {checked:true} : {checked:false}}  onChange={()=>dispatch(uiActions.toggleThemeMode())}/>} label="Dark mode" />
     </FormGroup>
     </Box>
   </Drawer>
