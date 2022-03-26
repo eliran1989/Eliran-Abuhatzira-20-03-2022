0a7e5c8703c3edc3f59cb3c7b3abbfa55dd7f14e
Eliran Abuhatzira <elirana1989@gmail.com>
2022-03-26 17:48:56 +0300

    Set default location from device location by geolocation API.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import  React  from 'react';
-import { useSelector } from 'react-redux';
+import  React, { useEffect }  from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
 import Header from './components/UI/Header/Header'
-import {BrowserRouter,Routes,Route, useLocation} from "react-router-dom";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import { Alert, Slide, Snackbar } from '@mui/material';
+import { forecastActions } from './store/forecast-slice';
+import { apiKey } from './apiKey';
+import { uiActions } from './store/ui-slice';
 
 
 
@@ -18,11 +21,48 @@ function App(props) {
   const uiState = useSelector((state) => state.ui);
 
 
+   const dispatch = useDispatch();
+
   const theme = createTheme({
     palette: {
       mode: uiState.themeMode
     },
   })
+
+
+  useEffect(() => {
+
+      if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((position)=>{
+
+          fetch(`https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=${position.coords.latitude},${position.coords.longitude}`).then(res=>res.json()).then(response=>{
+
+          
+            dispatch(
+              forecastActions.changeCity({
+                cityName:response.EnglishName,
+                key:response.Key
+              })
+            )
+
+          }).catch(error=>{
+
+            dispatch(
+              uiActions.setError({
+                  errorMsg:error.toString()
+              })
+            )
+
+          })
+
+
+
+        });
+      }
+
+  }, [])
+  
+
 
 
   
